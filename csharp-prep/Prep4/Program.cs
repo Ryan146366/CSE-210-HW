6c0ba8a5d39a5d9621e3be4890740cfa3ad1efd6
Ryan146366 <122189512+Ryan146366@users.noreply.github.com>
2023-01-11 12:40:15 -0700

    Add files via upload

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int user_number = -1;
+        int sum = 0;
+        int max = 0;
+
+
+
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        while (user_number != 0)
+        {
+            Console.Write("Enter Number: ");
+            user_number = Convert.ToInt32(Console.ReadLine());
+            numbers.Add(user_number);
+
+
+        }
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > max)
+            {
+                max = number;
+            }
+            
+
+        }
+        int size = numbers.Count;
+        float average = sum / size;
+        
+        Console.WriteLine($"The Sum is: {sum}");
+        Console.WriteLine($"The Average is: {average}");
+        Console.WriteLine($"The Largest Number is {max}");
+
+
+
     }
+
+
 }
