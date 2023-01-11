3fe42795660a3942630fd2465efe5c16ece642e2
Ryan146366 <122189512+Ryan146366@users.noreply.github.com>
2023-01-11 13:23:38 -0700

    Add files via upload

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,42 @@ using System;
 
 class Program
 {
+   
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+
+     }
+    static int PromptUserNumber()
+     {
+        Console.WriteLine("Please enter your favorite number");
+        int fav_number = Convert.ToInt32(Console.ReadLine());
+
+        return fav_number;
+    }
+    static int SquareNumber(int fav_number)
+    {
+        int square = fav_number * fav_number;
+
+        return square;
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.Write($"{name}, The square of your number is {square}");
+     }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+     DisplayWelcome();
+     string username = PromptUserName();
+     int number = PromptUserNumber();
+     int square = SquareNumber(number);
+     DisplayResult(username, square);  
     }
 }
